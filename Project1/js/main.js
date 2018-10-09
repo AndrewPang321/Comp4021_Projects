8c6c2fb5f3829824c653af0e0c912a8ad5835f2c
AndrewPang <thpangab@connect.ust.hk>
2018-10-09 16:57:46 +0800

    Deleted redundant project

--- Project1/js/main.js
+++
@@ -1,8 +0,0 @@
-// Make the player jump by starting the player animation
-function jump() {
-    $("#bird").css("animationPlayState", "running");
-}
-
-function startgame(){
-    document.getElementById("title").style.display = "none";
-}
